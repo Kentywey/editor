0c8e55076e039247eb0ef7dc09204e363fc668e4
Kentywey <kweymeesch@gmail.com>
2017-11-16 14:42:44 +0100

    Exo 4 Singleton

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -2,4 +2,26 @@ package org.ulco;
 
 public class ID {
     static public int ID = 0;
+    private static ID INSTANCE = null;
+    private ID(){
+
+    }
+
+    public int currentId(){
+        return ID;
+    }
+
+    public int nextId(){
+        return ++ID;
+    }
+
+    private static ID getInstance(){
+        if (INSTANCE ==null){
+            INSTANCE = new ID();
+        }
+        return INSTANCE;
+    }
+
+
+
 }
